857b26e636cdf83716045608b45e2a38e4fe1d08
Kelven Galvão <32758755+irvine5k@users.noreply.github.com>
2021-05-29 11:03:27 -0300

    Separate Customer and Purchase at Repository Layer

--- lib/src/data/repositories/customer_repository.dart
+++ lib/src/data/repositories/customer_repository.dart
@@ -1,13 +1,17 @@
 import 'package:graphql/client.dart';
 import 'package:marketplace/src/data/models/customer_model.dart';
-import 'package:marketplace/src/data/models/purchase_response_model.dart';
 import 'package:marketplace/src/data/queries/marketplace_queries.dart';
 
-class CustomerRepository {
+abstract class CustomerRepository {
+  Future<CustomerModel> getCustomer();
+}
+
+class GraphQLCustomerRepository implements CustomerRepository {
   final GraphQLClient _client;
 
-  CustomerRepository(this._client);
+  GraphQLCustomerRepository(this._client);
 
+  @override
   Future<CustomerModel> getCustomer() async {
     final result = await _client.query(
       QueryOptions(document: gql(Queries.customer)),
@@ -21,22 +25,6 @@ class CustomerRepository {
       final customer = CustomerModel.fromJson(data);
 
       return customer;
-    }
-  }
-
-  Future<PurchaseResponseModel> purchase(String offerId) async {
-    final result = await _client.mutate(
-      MutationOptions(document: gql(Mutations.purchase(offerId))),
-    );
-
-    if (result.hasException) {
-      throw Exception();
-    } else {
-      final data = result.data!['purchase'];
-
-      final purchaseResponse = PurchaseResponseModel.fromJson(data);
-
-      return purchaseResponse;
     }
   }
 }
---
+++ lib/src/data/repositories/purchase_repository.dart
@@ -0,0 +1,30 @@
+import 'package:graphql/client.dart';
+import 'package:marketplace/src/data/models/purchase_response_model.dart';
+import 'package:marketplace/src/data/queries/marketplace_queries.dart';
+
+abstract class PurchaseRepository {
+  Future<PurchaseResponseModel> purchase(String offerId);
+}
+
+class GraphQLPurchaseRepository implements PurchaseRepository {
+  final GraphQLClient _client;
+
+  GraphQLPurchaseRepository(this._client);
+
+  @override
+  Future<PurchaseResponseModel> purchase(String offerId) async {
+    final result = await _client.mutate(
+      MutationOptions(document: gql(Mutations.purchase(offerId))),
+    );
+
+    if (result.hasException) {
+      throw Exception();
+    } else {
+      final data = result.data!['purchase'];
+
+      final purchaseResponse = PurchaseResponseModel.fromJson(data);
+
+      return purchaseResponse;
+    }
+  }
+}
