c0dbd82697e3b4628cd331667cd7391324a22ecf
Kelven Galvão <32758755+irvine5k@users.noreply.github.com>
2021-05-29 18:15:16 -0300

    Add custom dialog test

---
+++ test/src/common/utils/custom_dialog_test.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:marketplace/src/common/utils/utils.dart';
+import 'package:marketplace/src/common/widgets/text_button_widget.dart';
+
+final _alertDialogTitle = 'Alert Dialog';
+
+void main() {
+  testWidgets(
+    'When tap custom dialog button '
+    'should execute the callback',
+    (tester) async {
+      var clicked = false;
+
+      await _createWidget(tester, onPressed: () {
+        clicked = true;
+      });
+
+      await tester.tap(find.byType(TextButtonWidget));
+
+      await tester.pumpAndSettle();
+
+      await tester.tap(find.text('OK'));
+
+      expect(clicked, true);
+    },
+  );
+}
+
+Future<void> _createWidget(
+  WidgetTester tester, {
+  VoidCallback? onPressed,
+}) async {
+  await tester.pumpWidget(
+    MaterialApp(
+      home: Scaffold(
+        body: _Widget(
+          onPressed: onPressed,
+        ),
+      ),
+    ),
+  );
+}
+
+class _Widget extends StatelessWidget {
+  final VoidCallback? onPressed;
+
+  const _Widget({Key? key, this.onPressed}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return TextButtonWidget(
+      onTap: () => Utils.showCustomDialog(
+        context,
+        title: _alertDialogTitle,
+        onPressed: onPressed ?? () {},
+      ),
+      label: 'Button',
+    );
+  }
+}
