8b58614c80126b2c7727e6f125ce2c430c03ead3
Kelven Galvão <32758755+irvine5k@users.noreply.github.com>
2021-05-31 20:41:23 -0300

    Add Messages Object with All Strings in the App

---
+++ lib/src/common/resources/messages.dart
@@ -0,0 +1,15 @@
+class AppMessages {
+  static String greeting(String name) => 'Hi, $name';
+  static String buyNow(String value) => 'Buy now $value';
+  static const String success = 'Success';
+  static const String error = 'Error';
+  static const String moreButton = 'More';
+  static const String serverError = 'Something went wrong, try again, later.';
+  static const String backToHome = 'Back to Home';
+  static const String offerPageAppBarTitle = 'Offer Details';
+  static const String totalBalance = 'Total Balance';
+  static const String offersLabel = 'Offers';
+  static const String okButton = 'OK';
+  static const String somethingWrong = 'Something Wrong Happened';
+  static const String tryAgain = 'Try again';
+}
